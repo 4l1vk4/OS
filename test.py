65d8fad0c8dc4caa7248591db0378f11ce831c23
4l1vk4 <alivkatop@mail.ru>
2026-02-19 11:52:05 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,42 @@
+import ctypes
+import sys
+
+try:
+    lib = ctypes.CDLL("./libcaesar.so")
+except OSError:
+    print("Ошибка загрузки библиотеки")
+    sys.exit(1)
+
+# Указываем типы аргументов
+lib.caesar_key.argtypes = [ctypes.c_ubyte]
+lib.caesar_key.restype = None
+
+lib.caesar.argtypes = [
+    ctypes.c_void_p,
+    ctypes.c_void_p,
+    ctypes.c_int
+]
+lib.caesar.restype = None
+
+
+# Исходные данные
+data = b"hello world"
+length = len(data)
+
+# Создаём буфер для результата
+src = ctypes.create_string_buffer(data)
+dst = ctypes.create_string_buffer(length)
+
+# Устанавливаем ключ
+lib.caesar_key(3)
+
+# Шифруем
+lib.caesar(src, dst, length)
+
+print("Encrypted:", bytes(dst.raw))
+
+# Дешифруем обратно
+decrypted = ctypes.create_string_buffer(length)
+lib.caesar(dst, decrypted, length)
+
+print("Decrypted:", bytes(decrypted.raw))
